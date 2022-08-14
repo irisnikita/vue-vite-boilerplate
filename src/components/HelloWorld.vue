3c76361c69ab9fcc70cf961cf2e44a7c25ea4658
Vi Nguyen Luong Truong <vinlt@antsomi.com>
2022-08-14 15:21:58 +0700

    Fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,7 +18,7 @@
   </div>
 
   <p>
-  Check out
+    Check out 123
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
